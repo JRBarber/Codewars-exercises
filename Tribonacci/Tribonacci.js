0b0bdefeee16e429ad4b5a6137076bf56ed6099b
Joshua Barber <joshua.barber@protonmail.com>
2022-01-29 20:26:58 +0000

    Initial

---
+++ Tribonacci/Tribonacci.js
@@ -0,0 +1,28 @@
+function tribonacci(signature, n) {
+  const result = [];
+  result.length = n;
+  result.fill(0);
+
+  if (n > signature.length) {
+    var a = signature[0];
+    var b = signature[1];
+    var c = signature[2];
+
+    result[0] = a;
+    result[1] = b;
+    result[2] = c;
+
+    for (i = signature.length; i < n; i++) {
+      result[i] = result[i - 3] + result[i - 2] + result[i - 1];
+      //sum = a + b + c;
+      //result[i] = sum;
+      //a=b;
+      //b=c;
+    }
+  } else {
+    for (i = 0; i < n; i++) {
+      result[i] = signature[n];
+    }
+  }
+  return result;
+}
